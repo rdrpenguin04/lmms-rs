09d09f99e02e772867b9ef1d669203bdac37b9d4
Ray Redondo <rdrpenguin04@gmail.com>
2021-11-24 13:10:49 -0600

    More UI progress; add tasks.json for VSCode

--- gui/build.rs
+++ gui/build.rs
@@ -1,5 +1,6 @@
 #[cfg(feature = "gui_sixtyfps")]
 fn main() -> Result<(), sixtyfps_build::CompileError> {
+    println!("cargo:rerun-if-changed=ui/sixtyfps/main.60");
     sixtyfps_build::compile("ui/sixtyfps/main.60")
 }
 
--- gui/src/gui/sixtyfps.rs
+++ gui/src/gui/sixtyfps.rs
@@ -1,5 +1,19 @@
 sixtyfps::include_modules!();
 
+use sixtyfps::{ModelHandle, VecModel};
+use std::rc::Rc;
+
 pub fn start() {
-    MainWindow::new().run();
+    let window = MainWindow::new();
+    let tracks = VecModel::default();
+    let patterns = VecModel::default();
+    patterns.push((TrackPattern {
+        length: 1.0,
+        notes: ModelHandle::default(),
+    }, 0.0));
+    tracks.push(Track {
+        patterns: ModelHandle::new(Rc::new(patterns)),
+    });
+    window.set_tracks(ModelHandle::new(Rc::new(tracks)));
+    window.run();
 }
